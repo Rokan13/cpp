e9cf4f030ccbf4b7a0371a4c4d76e689686511ca
Shah Rokan <121371680+Rokan13@users.noreply.github.com>
2025-09-11 03:04:54 +0600

    Update default+setter+show.cpp

--- oop-mid/default+setter+show.cpp
+++ oop-mid/default+setter+show.cpp
@@ -6,20 +6,20 @@ private:
     string name;
     int age;
     public:
-        person(){name="none";age=00;} //just use dafault constructor and seta new value
+        person(){name="none";age=00;}    //just use dafault constructor and set a new value
 
         string getName(){return name;}
         int getAge(){return age;}
 
-        void setName(string n){name=n;}
+        void setName(string n){name=n;}   // u can only use setter function when there was a default value or you need to change value or need to set new value
         void setAge(int a){age=a;}
 };
 int main()
 {
   
     person p2;
-    p2.setName("rokan");  //set value
+    p2.setName("rokan");  // there was a default value, thats why we set value
     p2.setAge(23);
 
-cout<<"Name="<<p2.getName()<<"Age="<<p2.getAge()<<endl; //print or get new value
+cout<<"Name="<<p2.getName()<<"Age="<<p2.getAge()<<endl;    //print or get new value
 }
