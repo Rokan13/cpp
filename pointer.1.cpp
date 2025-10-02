0c521fccef19e930d5c3369c0c8bafd83917a275
Shah Rokan <shahrokan3@gmail.com>
2025-10-02 22:06:26 +0600

    pointer

---
+++ pointer.1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+ class myclass{
+ int a;
+ public:
+     myclass(int x);
+     int get();
+
+ };
+
+ myclass::myclass(int x)
+ {
+     a=x;
+ }
+
+ int myclass::get()
+ {
+     return a;
+ }
+
+ int main()
+ {
+      myclass ob(120);
+      myclass *p;
+      p=&ob;
+
+      cout<<"value using object:"<<ob.get();
+      cout<<"\n";
+      cout<<"value using pointer: " <<p->get();
+ }
