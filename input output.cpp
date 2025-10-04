a0ab43aa51fa6c4a118328523e1e6b30353cbaf0
Shah Rokan <shahrokan3@gmail.com>
2025-10-04 21:17:01 +0600

    input and output

---
+++ input output.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int cls=8;
+    long long  roll=55103043;
+    string name="Jeon";
+    string behave="picci";
+
+
+
+cout << "name "<< name<< endl;
+cout<<behave<<endl;
+
+}
