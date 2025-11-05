71eec7673fd707ba0ae25f331372c4e58183e52a
Shah Rokan <shahrokan3@gmail.com>
2025-11-06 01:41:07 +0600

    made by shimul for hotel managent with helped by chatgpt

---
+++ shimul.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int sum=0;
+   int t;
+   cin>>t;
+
+   while(t--)
+   {
+       int x,y;
+       cin>>x >>y;
+
+
+    sum=(x*4)+y;
+
+   cout<<sum;
+}
+}
