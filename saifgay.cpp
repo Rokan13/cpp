7036a4eae90a30a3d16fb8a6cd2de4e5c1bd9ce8
Shah Rokan <shahrokan3@gmail.com>
2025-11-09 23:28:21 +0600

    from saif

---
+++ saifgay.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin>> n;
+        vector<int> v(n);
+        for (auto& e : n)
+            cin >> n;
+
+        for(int i : range(100))
+        {
+            cin>
+        }
+    }
+}
